920ef5ae4aa6121c5eadb29ea0c9a36c8fd1b96b
Reza Shams <rezasa@axis.com>
2015-05-11 09:37:09 +0200

    Fixed todo.txt configuration on mac

--- configs/configure.sh
+++ configs/configure.sh
@@ -25,6 +25,8 @@ ln -s $HOME/Dropbox/Apps/todotxttdi/todo.sh $HOME/bin/todo.sh
 mkdir -p $HOME/.todo
 rm $HOME/.todo/config
 ln -s $HOME/Dropbox/Apps/todotxttdi/todo.cfg $HOME/.todo/config
+rm -rf $HOME/git/todo
+ln -s $HOME/Dropbox/Apps/todotxttdi/ $HOME/git/todo
 echo "todo.text configuration linked"
 
 #######
