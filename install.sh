54e9efd053bbcbf4baaa2fb06fdd2aef87b0d95e
Reza Shams Amiri <rezasa@axis.com>
2015-11-02 08:03:38 +0100

    Install.sh updated

--- install.sh
+++ install.sh
@@ -12,6 +12,17 @@ ln -s ~/git/MyVimConfig/extras/dircolors ~/.dircolors
 # linux specific
 #---------------
 
-# install ctags
+# install vim stuff
 sudo apt-get install ctags
+
+#install other stuff
+sudo apt-get install fortune
+sudo apt-get install grc
+sudo apt-get install pal
+
+#install cowsay
+wget http://ftp.acc.umu.se/mirror/cdimage/snapshot/Debian/pool/main/c/cowsay/cowsay_3.03.orig.tar.gz
+tar xf cowsay_3.03.orig.tar.gz
+cd cowsay-3.03
+./install.sh
 
