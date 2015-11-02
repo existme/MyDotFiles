95fff4e3dd1d47c4f3e3fea08f9cb592887670f0
Reza Shams Amiri <rezasa@axis.com>
2015-11-02 07:58:00 +0100

    Start to build automated install
    - renamed makelink.bash to install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,13 @@ rm ~/.gvimrc
 ln -s ~/git/MyVimConfig/vimrc ~/.vimrc
 ln -s ~/git/MyVimConfig/vim ~/.vim
 ln -s ~/git/MyVimConfig/gvimrc ~/.gvimrc
+ln -s ~/git/MyVimConfig/extras/dircolors ~/.dircolors
+
+
+#---------------
+# linux specific
+#---------------
+
+# install ctags
+sudo apt-get install ctags
+
