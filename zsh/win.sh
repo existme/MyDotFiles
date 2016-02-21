fb865b3d1cab3d9cbf78385a53e05bb2931441ce
existme <existme@gmail.com>
2016-02-21 20:40:28 +0100

    Updated intellij (idea) launcher for windows config

--- zsh/win.sh
+++ zsh/win.sh
@@ -22,9 +22,11 @@ alias pwd="pwd;cc"
 export DISPLAY=localhost:0.0
 export MAVEN_OPTS="-Xms256m -Xmx512m"
 export M2HOME=/cygdrive/c/unix/apache-maven-3.3.9
+export IDEAPATH="/cygdrive/c/Program\ Files\ \(x86\)/JetBrains/IntelliJ\ IDEA\ 15.0.3/bin/idea.bat"
 alias sublime="/cygdrive/c/Program\ Files/Sublime\ Text\ 3/sublime_text.exe"
 alias start="cygstart"
 alias cyg-get="/cygdrive/c/cygwin64/setup-x86_64.exe -q -P"
 alias pygmentize="~/git/Pygments-2.1/pygmentize "
 alias atom="/cygdrive/c/Users/Reza/AppData/Local/atom/app-1.4.3/resources/cli/atom.cmd"
-alias idea="/cygdrive/c/Program\ Files\ \(x86\)/JetBrains/IntelliJ\ IDEA\ 15.0.3/bin/idea.bat"
+alias idea="$SCRIPTPATH/extras/scripts/ideawin"
+
