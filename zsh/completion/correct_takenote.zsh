d2cd77986465a0a9408991bae2cdca547f2607c2
Reza Shams Amiri <existme@gmail.com>
2017-12-27 16:07:52 +0100

    Still unfinished business with ZLE

--- zsh/completion/correct_takenote.zsh
+++ zsh/completion/correct_takenote.zsh
@@ -34,13 +34,13 @@ local all=$(find -L "$HOME/notes/" -print 2> /dev/null)
 local dir=$( find -L "$HOME/notes/" -print 2> /dev/null | fzf-tmux +m ) 
 SUFFIX=$dir
 # emulate -L zsh
-_message Reza     #works!
+# _message Reza${comp_hasarg}     #works!
 comp=$dir
 # local id=10
 # _description $dir id 'action'
 # _describe -t values value dir
 eval "$_comp_setup"
-# echo -n -e "${bR}Editing${bW}... $BUFFER$cZ "
+echo -n -e "${bR}Editing${bW}... $BUFFER$cZ "
 REPLY=$dir
 nmatches=1
 # zle -R "takenote "
@@ -48,6 +48,6 @@ compstate[insert]=3
 compstate[match]=$dir
 # echo $compstate
 _lastcomp=( $dir 0 $dir 1 ) 
-RBUFFER=$dir
-# compadd $dir 
+
+compadd $dir 
 return ret
