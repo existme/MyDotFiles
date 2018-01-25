321c6de34415d6c1b44fb4910935cd49d7711dbc
Reza Shams Amiri <existme@gmail.com>
2018-01-25 01:32:54 +0100

    updated POWERLEVEL9K configuration

--- todo.md
+++ todo.md
@@ -23,5 +23,13 @@
 6. neomut:
 	1. move config to repo
 7. powerlevel:
-	1. change git section
+	1. (done) change git section
+   2. change color for ram and load
+   3. what other modules are available?
+8. vim:
+   1. <c-s> should work in paste mode as well
 
+
+# fun
+
+1. search github foro fzf repos
--- zsh/linux.sh
+++ zsh/linux.sh
@@ -27,10 +27,10 @@ unset dist
 if [[ $OTERM == 'xterm' || $OTERM == 'xterm-256color' || $OTERM == 'rxvt-256color' || $OTERM == 'screen-256color' ]] && ! isSSH ; then
 	# source $SCRIPTPATH/zsh/shell-prompt.sh
    source $SCRIPTPATH/zsh/powerthemes/reza
-	source $SCRIPTPATH/zsh/bundle/powerlevel9k/powerlevel9k.zsh-theme
 	add-zsh-hook -Uz precmd xterm_title_precmd
 	add-zsh-hook -Uz preexec xterm_title_preexec
 	# echo "--- Shell prompt loaded ---"
 else
 	export ZSH_THEME="agnoster"
 fi
+
