3435a8530850b55c5823939b959fec23186bcd58
Reza Shams Amiri <existme@gmail.com>
2018-02-14 22:55:06 +0100

    removed double defined functions

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -1,7 +1,13 @@
-# vim: set filetype=zsh 
+# vim: set filetype=zsh
 
 echoMe $0
 
+# undeclare a function prevent warnings if it wasn't defined before
+function unfunc(){
+   unset -f $1 &>/dev/null
+}
+
+unfunc isSSH
 isSSH(){
    export SESSION_TYPE=""
    if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
@@ -15,20 +21,22 @@ isSSH(){
    [ "$SESSION_TYPE" = "ssh" ] && return 0 || return 1
 }
 
+unfunc stackMsg
 stackMsg(){
   stack+=( $1 )
   export stack
 }
 
+unfunc echoMsgs
 echoMsgs(){
-  echo  
-  echo "${cW}░▒▓   ${cZ}"  
+  echo
+  echo "${cW}░▒▓   ${cZ}"
   local bar="${bgColorBorder}║${reset_color}"
-  echo "${cW}╔══════╦══════╦══════╦══════╦══════╗${cZ}"  
+  echo "${cW}╔══════╦══════╦══════╦══════╦══════╗${cZ}"
   echo ${bar====bar}
   echo "${bgColorBorder}╚══════╩══════╩══════╩══════╩══════╝${reset_color}"
   # local line="${bgColorBorder}╠══════╬══════╬══════╬══════╬══════╣${reset_color}"
- 
+
   for s in $stack
   do
     echo $s
@@ -36,6 +44,7 @@ echoMsgs(){
   unset stack
 }
 
+unfunc cdUndoKey
 cdUndoKey() {
   popd      > /dev/null
   zle       reset-prompt
@@ -44,6 +53,7 @@ cdUndoKey() {
   echo
 }
 
+unfunc cdParentKey
 cdParentKey() {
   pushd .. > /dev/null
   zle      reset-prompt
@@ -52,30 +62,36 @@ cdParentKey() {
   echo
 }
 
+unfunc deleteLine
 deleteLine(){
- 	zle     	kill-whole-line 
+ 	zle     	kill-whole-line
    zle      reset-prompt
 }
+
+unfunc deleteToStart
 deleteToStart(){
- 	zle   	backward-kill-line 
+ 	zle   	backward-kill-line
    zle      reset-prompt
 }
 
 
-function rgrep(){ 
+unfunc rgrep
+function rgrep(){
   grep --color=always -R -i "$1" *;
 }
 
-function rfind(){ 
+unfunc rfind
+function rfind(){
    if [[ $1 == "--help" || $# -eq 0 ]]; then
       echo "Usage: rfind readme ~/git/MyDotFiles"
       return
    fi
    echo "find $2 -iname \"*$1*\"|grep -i \"$1\" --color=always"
    find $2 -iname "*$1*"|grep -i "$1" --color=always
-} 
+}
 # A macro to see the contents of a jar or war file
-function lessj(){ 
+unfunc lessj
+function lessj(){
 	if [[ -z $1 ]]; then
 		jar tvf *.jar|pygmentize -f "terminal" -l sv -f terminal256 -s|less
 	else
@@ -83,15 +99,16 @@ function lessj(){
 	fi
 }
 # easy archive extraction either use extract x.tar or extract x.tar "/your/destination
+unfunc extract
 function extract(){
 	if [[ -z ${2+x} ]]; then
 		tar xf $1;
 	else
 		tar xf $1 -C $2;
 	fi
-} 
+}
 
-
+unfunc __is_slow_storage
 #Return “yes” if the repository is on an “nfs” mount
 function __is_slow_storage() {
    export STAT_OPT='-L --file-system --format="%T"'
@@ -106,6 +123,8 @@ function __is_slow_storage() {
       echo "no"
    fi
 }
+
+unfunc git_prompt_info
 # Fix  oh-myzsh git prompt slowness issue for some repos
 function git_prompt_info() {
   local ref
@@ -121,32 +140,35 @@ function git_prompt_info() {
 # }
 
 # Search for all installed packages
+unfunc as
 function as(){
    local p=$(apt-cache search . |fzf -e -x --algo=v2 +m --cycle --print-query -q "$1" |tail -n 1|cut -d' ' -f 1)
-   echo $p
-   { apt show $p ; echo "--------[ files ]-------" ; dpkg-query --listfiles $p } | vim +'nnoremap q :q!<enter>' +'set tm=0' +'setf dts' -R -
+   [ -n "$p" ] && \
+      { apt show $p ; echo "--------[ files ]-------" ; dpkg-query --listfiles $p } | vim +'nnoremap q :q!<enter>' +'set tm=0' +'setf dts' -R -
 }
 
 # Search through all installed files via packages
+unfunc asf
 function asf(){
    local p=$(dpkg -S "*"|fzf -e -x --algo=v2 +m --cycle --print-query -q "$1" --prompt "Executable name:" |tail -n 1|cut -d':' -f 1)
-   echo $p
-   { apt show $p ; echo "--------[ files ]-------" ; dpkg-query --listfiles $p } | vim +'nnoremap q :q!<enter>' +'set tm=0' +'setf dts' -R -
+   [ -n "$p" ] && \
+      { apt show $p ; echo "--------[ files ]-------" ; dpkg-query --listfiles $p } | vim +'nnoremap q :q!<enter>' +'set tm=0' +'setf dts' -R -
 }
 
 # ex open file
-# Opens a binary using $1 supplying 
+# Opens a binary using $1 supplying
 #
 # ex: alias gg="_ex '/usr/bin/geany' '&>/dev/null &' "
+unfunc _ex
 function _ex(){
    unsetopt multios
    cmd=$1                                          # The actual command to be executed
-   postcmd=$2                                      # could be used for silencing and forking like '&>/dev/null &' 
+   postcmd=$2                                      # could be used for silencing and forking like '&>/dev/null &'
    param=${3:-}                                    # Filename or any prameter
 
    echo "\n${bP}AliasRunner${cZ}: ${bR}$cmd ${bW}$param ${bB}$postcmd${cZ}"
    (eval "$cmd $param $postcmd")                   # using subshell to hide job information
-} 
+}
 
 # Gamma handling for future use
 # function increase_gamma() {
@@ -160,7 +182,7 @@ function _ex(){
 #     (( GAMMA_CURRENT=$GAMMA_TEMP-0.1 ))
 #     xrandr --output $DP0 --gamma $GAMMA_CURRENT:$GAMMA_CURRENT:$GAMMA_CURRENT
 # }
-
+unfunc y
 function y(){
    if [[ $1 == "--help" || $# -eq 0 ]]; then
       echo "Usage: y i3"
@@ -171,19 +193,21 @@ function y(){
 
 # return file path
 #
-# Auto 
+# Auto
+unfunc p
 function p(){
     echo `which $1`
 }
 
 # view files in path
 #
-# Auto 
+# Auto
+unfunc vf
 function vf(){
-    declare local filepath=$(p $1)    
-    if [[ $filepath =~ (not found)$ ]]; then      
+    declare local filepath=$(p $1)
+    if [[ $filepath =~ (not found)$ ]]; then
       (
-        # It should be done like this otherwise IFS doesn't work        
+        # It should be done like this otherwise IFS doesn't work
         IFS=: declare -a local arr=($=PATH)
         # Add home to path
         arr+=("$HOME")
@@ -194,8 +218,8 @@ function vf(){
             grc file $file
             grc ls -la $file
             # removed -q because then the deault will always be n
-            read -k 1 -rs "pr?Do you want to edit it? (${bG}Y${cZ}/${bW}n${cZ})" 
-            echo 
+            read -k 1 -rs "pr?Do you want to edit it? (${bG}Y${cZ}/${bW}n${cZ})"
+            echo
             if [[ $pr =~ ^[Nn]$ ]]; then
               exit
             fi
@@ -210,7 +234,7 @@ function vf(){
       grc file $filepath
       grc ls -la $filepath
     fi
-    
+
     if  [ -z "$(file $filepath|grep -E 'ASCII|UTF')" ]; then
         echo "${vR}Binary$cZ file."
         return
@@ -219,122 +243,16 @@ function vf(){
 }
 
 # Add a path if it already doesn't exists
-pathadd() {                                                                      
-    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then                          
-        export PATH="${PATH:+"$PATH:"}$1"                                        
-    fi                                                                           
-} 
-
-
-# vim: set filetype=zsh 
-
-echoMe $0
-
-isSSH(){
-   export SESSION_TYPE=""
-   if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-     export SESSION_TYPE=ssh
-   # many other tests omitted
-   else
-     case $(ps -o comm= -p $PPID) in
-       sshd|*/sshd) export SESSION_TYPE=ssh;;
-     esac
-   fi
-   [ "$SESSION_TYPE" = "ssh" ] && return 0 || return 1
-}
-
-stackMsg(){
-  stack+=( $1 )
-  export stack
-}
-
-echoMsgs(){
-  echo  
-  echo "${cW}░▒▓   ${cZ}"  
-  local bar="${bgColorBorder}║${reset_color}"
-  echo "${cW}╔══════╦══════╦══════╦══════╦══════╗${cZ}"  
-  echo ${bar====bar}
-  echo "${bgColorBorder}╚══════╩══════╩══════╩══════╩══════╝${reset_color}"
-  # local line="${bgColorBorder}╠══════╬══════╬══════╬══════╬══════╣${reset_color}"
- 
-  for s in $stack
-  do
-    echo $s
-  done
-  unset stack
-}
-
-cdUndoKey() {
-  popd      > /dev/null
-  zle       reset-prompt
-  echo
-  ls
-  echo
-}
-
-cdParentKey() {
-  pushd .. > /dev/null
-  zle      reset-prompt
-  echo
-  ls
-  echo
-}
-
-deleteLine(){
- 	zle     	kill-whole-line 
-   zle      reset-prompt
-}
-deleteToStart(){
- 	zle   	backward-kill-line 
-   zle      reset-prompt
+unfunc pathadd
+pathadd() {
+    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
+        export PATH="${PATH:+"$PATH:"}$1"
+    fi
 }
 
 
-function rgrep(){ 
-  grep --color=always -R -i "$1" *;
-}
-
-function rfind(){ 
-   if [[ $1 == "--help" || $# -eq 0 ]]; then
-      echo "Usage: rfind readme ~/git/MyDotFiles"
-      return
-   fi
-   echo "find $2 -iname \"*$1*\"|grep -i \"$1\" --color=always"
-   find $2 -iname "*$1*"|grep -i "$1" --color=always
-} 
-# A macro to see the contents of a jar or war file
-function lessj(){ 
-	if [[ -z $1 ]]; then
-		jar tvf *.jar|pygmentize -f "terminal" -l sv -f terminal256 -s|less
-	else
-		jar tvf $1|pygmentize -f "terminal" -l sv -f terminal256 -s|less
-	fi
-}
-# easy archive extraction either use extract x.tar or extract x.tar "/your/destination
-function extract(){
-	if [[ -z ${2+x} ]]; then
-		tar xf $1;
-	else
-		tar xf $1 -C $2;
-	fi
-} 
-
-
-#Return “yes” if the repository is on an “nfs” mount
-function __is_slow_storage() {
-   export STAT_OPT='-L --file-system --format="%T"'
-   if [ $OSTYPE == "Darwin" ]; then
-      STAT_OPT='-L -f "%HT"'
-   fi
-   RES=$(stat `echo $STAT_OPT` `echo $1`)
-
-   if [[ $RES == "nfs" || $RES == "cifs" ]]; then
-      echo "yes"
-   else
-      echo "no"
-   fi
-}
 # Fix  oh-myzsh git prompt slowness issue for some repos
+unfunc git_prompt_info
 function git_prompt_info() {
   local ref
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
@@ -348,28 +266,6 @@ function git_prompt_info() {
 #    echo !!
 # }
 
-# Search for all installed packages
-function as(){
-   local p=$(apt-cache search . |fzf -e -x --algo=v2 +m --cycle --print-query -q "$1" |tail -n 1|cut -d' ' -f 1)
-   echo $p
-   { apt show $p ; echo "--------[ files ]-------" ; dpkg-query --listfiles $p } | vim +'nnoremap q :q!<enter>' +'set tm=0' +'setf dts' -R -
-}
-
-# Search through all installed files via packages
-function asf(){
-   local p=$(dpkg -S "*"|fzf -e -x --algo=v2 +m --cycle --print-query -q "$1" --prompt "Executable name:" |tail -n 1|cut -d':' -f 1)
-   echo $p
-   { apt show $p ; echo "--------[ files ]-------" ; dpkg-query --listfiles $p } | vim +'nnoremap q :q!<enter>' +'set tm=0' +'setf dts' -R -
-}
-
-function y(){
-   if [[ $1 == "--help" || $# -eq 0 ]]; then
-      echo "Usage: y i3"
-      return
-   fi
-   yelp man:$1 &
-}
-
 # simple yes no function, sample usage:
 #
 # [[ $(yesno "${bR}smenu${cZ} is cool") ]] && echo "OK" || echo "NO"
@@ -377,7 +273,7 @@ function yesno(){
    local msg
    [[ -z $1 ]] && msg="Please confirm your choice:" || msg=$1
    echo "Result: "
-   # local RES=echo "YES" "NO" "CANCEL" | smenu -2 "^Y" -1 "^N" -3 "^C" -s "/^N" -m $msg 
+   # local RES=echo "YES" "NO" "CANCEL" | smenu -2 "^Y" -1 "^N" -3 "^C" -s "/^N" -m $msg
    echo "Result: $RES"
    if [[ "$RES" = "YES" ]]; then
       echo "${bR}Canceling{$bW}...${cZ}"
@@ -386,82 +282,23 @@ function yesno(){
    return 1
 }
 
-# return file path
-#
-# Auto 
-function p(){
-    echo `which $1`
-}
-
-# view files in path
-#
-# Auto 
-function vf(){
-    declare local filepath=$(p $1)    
-    if [[ $filepath =~ (not found)$ ]]; then      
-      (
-        # It should be done like this otherwise IFS doesn't work        
-        IFS=: declare -a local arr=($=PATH)
-        # Add home to path
-        arr+=("$HOME")
-        local file
-        for p in $arr; do
-          file=$p/$1
-          if [ -f "$file" ]; then
-            grc file $file
-            grc ls -la $file
-            # removed -q because then the deault will always be n
-            read -k 1 -rs "pr?Do you want to edit it? (${bG}Y${cZ}/${bW}n${cZ})" 
-            echo 
-            if [[ $pr =~ ^[Nn]$ ]]; then
-              exit
-            fi
-            vim $file
-            exit
-          fi
-        done
-        echo "File: ${bY}$1${cZ} ${bR}not found${cZ} in path!"
-      )
-      return
-    else
-      grc file $filepath
-      grc ls -la $filepath
-    fi
-    
-    if  [ -z "$(file $filepath|grep -E 'ASCII|UTF|Perl')" ]; then
-        echo "${vR}Binary$cZ file."
-        return
-    fi
-    vim $filepath
-}
-
-# Add a path if it already doesn't exists
-pathadd() {                                                                      
-    if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then                          
-        export PATH="${PATH:+"$PATH:"}$1"                                        
-    fi                                                                           
-} 
-
+unfunc mann
 mann() {
    local man="https://www.manned.org/$1"
    echo "${bY}Opening ${bW}$man${cZ} for {$bB}'$1'${cZ} command."
    xdg-open $man > /dev/null 2>&1
 }
 
+unfunc mank
 mank() {
    local man="https://www.mankier.com/1/$1"
    echo "${bY}Opening ${bW}$man${cZ} for {$bB}'$1'${cZ} command."
    xdg-open $man > /dev/null 2>&1
 }
 
+unfunc mann
 mann() {
    local man="https://www.manned.org/$1"
-   echo "${bY}Opening ${bW}$man${cZ} for {$bB}'$1'${cZ} command."
-   xdg-open $man > /dev/null 2>&1
-}
-
-mank() {
-   local man="https://www.mankier.com/1/$1"
    echo "${bY}Opening ${bW}$man${cZ} for {$bB}'$1'${cZ} command."
    xdg-open $man > /dev/null 2>&1
 }
