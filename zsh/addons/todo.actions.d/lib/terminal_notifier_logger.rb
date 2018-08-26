a75d33184b323fb6bcf2e3fd27bfdfc606e49c46
Reza Shams Amiri <rezasa@axis.com>
2018-08-26 19:20:51 +0200

    fixed todotxt/addp addon when replacing here and hostname

--- zsh/addons/todo.actions.d/lib/terminal_notifier_logger.rb
+++ zsh/addons/todo.actions.d/lib/terminal_notifier_logger.rb
@@ -1,11 +1,11 @@
 class TerminalNotifierLogger
   def notify_start task
     # TerminalNotifier.notify('Pomodoro started', title: 'Pomotxt', sound: 'Glass')
-    system("notify-send -c 'info' 'Pomodoro started' 'Pomotxt' -i dialog-information")
+    system("notify-send -c 'info' 'Pomodoro started' '#{task}' -i dialog-information")
   end
 
   def notify_completed task
     # TerminalNotifier.notify('Pomodoro completed!', title: 'Pomotxt', sound: 'Glass')
-    system("notify-send -c 'info' 'Pomodoro completed' 'Pomotxt' -i dialog-information")
+    system("notify-send -c 'info' 'Pomodoro completed' '#{task}' -i dialog-information")
   end
 end
