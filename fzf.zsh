74ccbee25017dcde96eb35113a54b6f2fed0697a
Reza Shams Amiri <rezasa@axis.com>
2019-01-27 20:16:01 +0100

    removed unnecessary script

--- fzf.zsh
+++ fzf.zsh
@@ -51,7 +51,6 @@ export FZF_DEFAULT_OPTS="
   --color=bg+:$color01,bg:$color00,spinner:$color0C,hl:$color0D
   --color=fg:$color07,header:$color0D,info:$color0A,pointer:$color0C
   --color=marker:$color0C,fg+:$color06,prompt:$color0A,hl+:$color08
-  --preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'
   --bind 'f2:execute(vim -)+abort'
   --ansi
 "
