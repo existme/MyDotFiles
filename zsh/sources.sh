4655ccad71f3a8a2a21279d3dff4acbf03a80ff0
Reza Shams Amiri <rezasa@axis.com>
2019-07-26 13:30:45 +0200

    submodule update

--- zsh/sources.sh
+++ zsh/sources.sh
@@ -53,7 +53,7 @@ fi
 [[ -f $HOME/zshrc.local.sh ]] &&	source $HOME/zshrc.local.sh && echoP "${bB}~/zshrc.local.sh${cZ}"
 
 # Add completion path before oh-my-zsh starts compinit
-export fpath=($SCRIPTPATH/zsh/completion $SCRIPTPATH/zsh/functions $fpath)
+fpath=($SCRIPTPATH/zsh/completion $SCRIPTPATH/zsh/functions $fpath)
 
 
 
